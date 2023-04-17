cdbabaeac2fce07ff581af0daa09744dba3b29cc
hirthiha <hirthiha.doodleblue@gmail.com>
2023-04-17 13:17:38 +0530

    Axios

---
+++ Api_integration/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import Flatlist from './src/flatlist';
+function App() {
+  return <Flatlist></Flatlist>;
+}
+
+export default App;
---
+++ Api_integration/src/flatlist.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import styles from './Styles';
+
+function Flatlist() {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default Flatlist;
